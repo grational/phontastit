befdb37f370f3bcbc90d48cbe3da8425cc0481ad
Giuseppe Ricupero <giuseppe.ricupero@gmail.com>
2018-08-31 11:52:42 +0200

    First working version of the library

---
+++ src/main/groovy/it/grational/phontastit/ItalianPhoneNumber.groovy
@@ -0,0 +1,70 @@
+package it.grational.phontastit
+
+/**
+ * PhontastIT
+ * This class recognize the type of Italian phone numbers using a heuristic
+ * @return The type of italiaon phone passed as a parameter
+ * @author grational
+ * @date 30-08-2018 20.34
+ */
+class ItalianPhoneNumber {
+	private final String  phone
+	private final Boolean fax
+
+	/**
+	 * Primary constructor
+	 *
+	 * @param ph  the Italian phone number to be rapresented by the object
+	 * @param fx  a boolean flag to force the type to FAX
+	 */
+	ItalianPhoneNumber (
+		String ph,
+		Boolean fx = false
+	) {
+		this.phone = ph?.replaceAll(/\s+/,'') ?: { throw new IllegalArgumentException("Empty or null phone number") }()
+		this.fax = fx
+	}
+
+	/**
+	 * Return the type of the phone number rapresented by the class
+	 *
+	 * The supported types are: LANDLINE, MOBILE, TOLLFREE, PREMIUM, FAX
+	 * Since the FAX phone type is not recognizable the external boolean
+	 * parameter fax is used to force this type
+	 *
+	 * @return PhoneNumberType  return an enum PhoneNumberType indicating the possible types
+	 */
+	PhoneNumberType type() {
+		this.fax ? PhoneNumberType.FAX : heuristic(this.phone)
+	}
+
+	/**
+	 * This method decides according to the starting digits of
+	 * an Italian Phone Number without the i18n IT prefix
+	 */
+	private PhoneNumberType heuristic(String phone) {
+		PhoneNumberType type
+		switch(localPhoneNumber(phone)) {
+			case ~/^0\d{8,9}$/:
+				type = PhoneNumberType.LANDLINE
+				break
+			case ~/^3[1234689]\d\d{6,7}$/:
+				type = PhoneNumberType.MOBILE
+				break
+			case ~/^80[03]\d+$/:
+				type = PhoneNumberType.TOLLFREE
+				break
+			case ~/^(178|199|840|848|892|893|894|895|899)\d+$/:
+				type = PhoneNumberType.PREMIUM
+				break
+			default:
+				throw new IllegalStateException("Cannot determine the number '${phone}' correctly" as String)
+		}
+		return type
+	}
+
+	private localPhoneNumber(String phone) {
+		return phone.replaceFirst(/^(?:00|\+)39/,'')
+	}
+
+}
